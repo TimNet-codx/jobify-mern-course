05101e7416c6127b01b9d7051e5a818e34a3c45c
TimNet <jesulayomitimze55@gmail.com>
2024-10-07 15:06:23 +0100

    fixed front end

--- server.js
+++ server.js
@@ -72,9 +72,13 @@ if (process.env.NODE_ENV === 'development') {
 
 //app.use(express.static(path.resolve(__dirname, './public')));
     // change to cloudinary for image storage
-app.use(express.static(path.resolve(__dirname, './public')));
+//app.use(express.static(path.resolve(__dirname, './public')));
+
 
 //app.use(express.static(path.resolve(__dirname, './public')));
+
+app.use(express.static(path.resolve(__dirname, './client/dist')));
+
 //app.use(morgan('dev'));
 app.use(cookieParser());
 app.use(express.json());
@@ -130,7 +134,7 @@ app.use('/api/v1/auth', authRouter);
 //console.log('Current directory:', process.cwd());
 
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './public', 'index.html'));
+  res.sendFile(path.resolve(__dirname, './client/dist', 'index.html'));
 });
 
 
