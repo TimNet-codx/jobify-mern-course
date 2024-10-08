e5f946b7a145b17f1f9b24d1625ef4a470ab58ed
TimNet <jesulayomitimze55@gmail.com>
2024-10-08 17:07:25 +0100

    fix update

--- middleware/multerMiddleware.js
+++ middleware/multerMiddleware.js
@@ -1,7 +1,9 @@
 import multer from "multer";
+import DataParser from 'datauri/parser.js';
+import path from 'path';
 
 // cb == call back
-const storage = multer.diskStorage({
+/*const storage = multer.diskStorage({
  destination: (req, file, cb) => {
     cb(null, 'public/uploads');
  },
@@ -10,7 +12,20 @@ const storage = multer.diskStorage({
     cb(null, fileName);
  },
 });
+// We change the  above because it don't work free Render when deploy it
+*/
 
+// We change the  above because it don't work free Render when deploy it
+// For file upload or picture upload
+const storage = multer.memoryStorage()
 const upload = multer({storage});
+
+const parser = new DataParser();
+
+export const formatImage = (file) => {
+  //console.log(file);
+  const fileExtension = path.extname(file.originalname).toString()
+  return parser.format(fileExtension, file.buffer).content;
+};
 
 export default upload;
